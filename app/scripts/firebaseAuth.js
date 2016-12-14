08552196d30fded1ab4a1865ececab39741811c4
Alyssa Broussard <alyssabroussard@legalshieldcorp.com>
2016-12-14 11:06:19 -0600

    Intro closes #1

---
+++ app/scripts/firebaseAuth.js
@@ -0,0 +1,9 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAxG-gQ_5qtpXEjvAx5K5glGeeOp_8j6iM",
+    authDomain: "bloc-chat-457c1.firebaseapp.com",
+    databaseURL: "https://bloc-chat-457c1.firebaseio.com",
+    storageBucket: "bloc-chat-457c1.appspot.com",
+    messagingSenderId: "215521011215"
+  };
+  firebase.initializeApp(config);
