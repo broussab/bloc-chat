1ea2d1bb2b07c099aeb17a826bd9da9fd5caa74b
Alyssa Broussard <alyssabroussard@legalshieldcorp.com>
2016-12-27 09:34:26 -0600

    User story closes # 4

--- app/scripts/Controllers/RoomCtrl.js
+++ app/scripts/Controllers/RoomCtrl.js
@@ -1,10 +1,20 @@
 (function() {
-     function RoomCtrl(Room, $scope) {
+    function RoomCtrl(Room, $scope, $uibModal) {
         this.heroTitle = "Available Rooms";
         $scope.chatRooms=Room.all;
-     }
- 
+         
+        this.openModal = function(){
+          var modal = $uibModal.open({
+            templateUrl: '/templates/modal.html',
+            controller: 'ModalCtrl',
+            controllerAs: 'modal'
+          });
+          modal.result.then(function (name){
+              Room.add(name);
+          });
+         }
+    }  
      angular
          .module('blocChat')
-         .controller('RoomCtrl', ['Room', '$scope', RoomCtrl]);
+         .controller('RoomCtrl', ['Room', '$scope', '$uibModal', RoomCtrl]);
  })();
